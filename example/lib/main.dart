e7214444c8e18cae770cfc5d0788987e26e3d86f
maheshjamdade <mahesh.jamdade@codemonk.in>
2020-12-19 19:00:35 +0530

    feat: Add Sample for client

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,43 +6,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter SearchField Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,83 +29,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<String> _suggestions = [
+    'United States',
+    'Germany',
+    'Washington',
+    'Paris',
+    'Jakarta',
+    'Australia',
+    'India',
+    'Czech Republic',
+    'Lorem Ipsum',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SearchField(
-                suggestions: [
-                  'United States',
-                  'America',
-                  'Washington',
-                  'Paris',
-                  'Jakarta',
-                  'Australia',
-                  'Lorem Ipsum'
-                ],
-                // initialValue: 'Lorem Ipsum',
+        body: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchField(
+                suggestions: _suggestions,
+                hint: 'SearchField Sample 1',
+                maxSuggestionsInViewPort: 3,
+                itemHeight: 45,
+                onTap: (x) {
+                  print(x);
+                },
               ),
-              SizedBox(height: 20),
-              Text(
-                'You have pushed the button this many times:',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchField(
+                suggestions: _suggestions,
+                hint: 'SearchField Sample 2',
+                searchStyle: TextStyle(
+                    fontSize: 18, color: Colors.black.withOpacity(0.8)),
+                searchInputDecoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.black.withOpacity(0.8)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    )),
+                maxSuggestionsInViewPort: 6,
+                itemHeight: 50,
+                onTap: (x) {
+                  print(x);
+                },
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchField(
+                  maxSuggestionsInViewPort: 5,
+                  itemHeight: 40,
+                  hint: 'SearchField Sample 3',
+                  suggestionsDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(8.0),
+                          bottomRight: Radius.circular(8)),
+                      border: Border.all(color: Colors.grey.withOpacity(0.5))),
+                  suggestionItemDecoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color(0xff9D50BB).withOpacity(0.5),
+                    Color(0xff6E48AA).withOpacity(0.8)
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                  searchInputDecoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.2),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.white, width: 2.0),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      border: OutlineInputBorder()),
+                  marginColor: Colors.white,
+                  suggestions: _suggestions),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SearchField(
+                suggestionStyle: TextStyle(color: Colors.green),
+                suggestionItemDecoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    gradient: LinearGradient(
+                        colors: [Color(0xff70e1f5), Color(0xffffd194)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight)),
+                suggestions: _suggestions,
+                searchInputDecoration: InputDecoration(
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    labelText: 'SearchField',
+                    border: OutlineInputBorder()),
+                hint: 'SearchField Sample 4',
+                maxSuggestionsInViewPort: 4,
+                itemHeight: 45,
+                onTap: (x) {
+                  print(x);
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
