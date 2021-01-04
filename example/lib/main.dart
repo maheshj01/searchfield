db96a02a465943d9a51ca918b90d9156dbc00f5e
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2021-01-04 19:46:27 +0530

    fix darkmode issue, improve formatting, add lints

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -45,7 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _searchController.dispose();
     super.dispose();
   }
@@ -74,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 maxSuggestionsInViewPort: 3,
                 itemHeight: 45,
                 onTap: (x) {
-                  print('selected =$x ' + _searchController.text);
+                  print('selected =$x ${_searchController.text}');
                 },
               ),
             ),
@@ -87,15 +86,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 suggestions: _suggestions,
                 hint: 'SearchField Sample 2',
                 searchStyle: TextStyle(
-                    fontSize: 18, color: Colors.black.withOpacity(0.8)),
+                  fontSize: 18,
+                  color: Colors.black.withOpacity(0.8),
+                ),
                 searchInputDecoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.black.withOpacity(0.8)),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black.withOpacity(0.8),
                     ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    )),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                ),
                 maxSuggestionsInViewPort: 6,
                 itemHeight: 50,
                 onTap: (x) {
@@ -109,30 +112,43 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: SearchField(
-                  maxSuggestionsInViewPort: 5,
-                  itemHeight: 40,
-                  hint: 'SearchField Sample 3',
-                  suggestionsDecoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(8.0),
-                          bottomRight: Radius.circular(8)),
-                      border: Border.all(color: Colors.grey.withOpacity(0.5))),
-                  suggestionItemDecoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                    Color(0xff9D50BB).withOpacity(0.5),
-                    Color(0xff6E48AA).withOpacity(0.8)
-                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-                  searchInputDecoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey.withOpacity(0.2),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            const BorderSide(color: Colors.white, width: 2.0),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      border: OutlineInputBorder()),
-                  marginColor: Colors.white,
-                  suggestions: _suggestions),
+                maxSuggestionsInViewPort: 5,
+                itemHeight: 40,
+                hint: 'SearchField Sample 3',
+                suggestionsDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8.0),
+                    bottomRight: Radius.circular(8),
+                  ),
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                ),
+                suggestionItemDecoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xff9D50BB).withOpacity(0.5),
+                      Color(0xff6E48AA).withOpacity(0.8),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+                searchInputDecoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.withOpacity(0.2),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                marginColor: Colors.white,
+                suggestions: _suggestions,
+              ),
             ),
             SizedBox(
               height: 20,
@@ -142,16 +158,18 @@ class _MyHomePageState extends State<MyHomePage> {
               child: SearchField(
                 suggestionStyle: TextStyle(color: Colors.green),
                 suggestionItemDecoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.2),
-                    gradient: LinearGradient(
-                        colors: [Color(0xff70e1f5), Color(0xffffd194)],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight)),
+                  color: Colors.grey.withOpacity(0.2),
+                  gradient: LinearGradient(colors: [
+                    Color(0xff70e1f5),
+                    Color(0xffffd194),
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                ),
                 suggestions: _suggestions,
                 searchInputDecoration: InputDecoration(
-                    floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    labelText: 'SearchField',
-                    border: OutlineInputBorder()),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  labelText: 'SearchField',
+                  border: OutlineInputBorder(),
+                ),
                 hint: 'SearchField Sample 4',
                 maxSuggestionsInViewPort: 4,
                 itemHeight: 45,
