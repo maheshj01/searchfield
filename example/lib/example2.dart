5b8d43d69b04b4bccf92f9359505c8a20edd027e
mahesh jamdade <maheshmn121@gmail.com>
2021-04-16 23:46:12 +0530

    chore: add example

---
+++ example/lib/example2.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:searchfield/searchfield.dart';
+
+class ExampleDemo extends StatefulWidget {
+  @override
+  _ExampleDemoState createState() => _ExampleDemoState();
+}
+
+class _ExampleDemoState extends State<ExampleDemo> {
+  final List<String> _suggestions = [
+    'United States',
+    'Germany',
+    'Washington',
+    'Paris',
+    'Jakarta',
+    'Australia',
+    'India',
+    'Czech Republic',
+    'Lorem Ipsum',
+  ];
+  final List<String> _statesOfIndia = [
+    'Andhra Pradesh',
+    'Assam',
+    'Arunachal Pradesh',
+    'Bihar',
+    'Goa',
+    'Gujarat',
+    'Jammu and Kashmir',
+    'Jharkhand',
+    'West Bengal',
+    'Karnataka',
+    'Kerala',
+    'Madhya Pradesh',
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Mizoram',
+    'Nagaland',
+    'Orissa',
+    'Punjab',
+    'Rajasthan',
+    'Sikkim',
+    'Tamil Nadu',
+    'Tripura',
+    'Uttaranchal',
+    'Uttar Pradesh',
+    'Haryana',
+    'Himachal Pradesh',
+    'Chhattisgarh'
+  ];
+  final _formKey = GlobalKey<FormState>();
+
+  final _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SearchField(
+            suggestions: _suggestions,
+            controller: _searchController,
+            hint: 'SearchField Sample 1',
+            initialValue: _suggestions[2],
+            maxSuggestionsInViewPort: 3,
+            itemHeight: 45,
+            onTap: (x) {
+              print('selected =$x ${_searchController.text}');
+            },
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: SearchField(
+              suggestions: _statesOfIndia,
+              hint: 'SearchField Sample 2',
+              searchStyle: TextStyle(
+                fontSize: 18,
+                color: Colors.black.withOpacity(0.8),
+              ),
+              validator: (x) {
+                if (!_statesOfIndia.contains(x) || x.isEmpty) {
+                  return 'Please Enter a valid State';
+                }
+                return null;
+              },
+              searchInputDecoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black.withOpacity(0.8),
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+              maxSuggestionsInViewPort: 6,
+              itemHeight: 50,
+              onTap: (x) {
+                print(x);
+              },
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SearchField(
+            maxSuggestionsInViewPort: 5,
+            itemHeight: 40,
+            hint: 'SearchField Sample 3',
+            suggestionsDecoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(8.0),
+                bottomRight: Radius.circular(8),
+              ),
+              border: Border.all(
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ),
+            suggestionItemDecoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff9D50BB).withOpacity(0.5),
+                  Color(0xff6E48AA).withOpacity(0.8),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            searchInputDecoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.grey.withOpacity(0.2),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.white,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              border: OutlineInputBorder(),
+            ),
+            marginColor: Colors.white,
+            suggestions: _suggestions,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SearchField(
+            suggestionStyle: TextStyle(color: Colors.green),
+            suggestionItemDecoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.2),
+              gradient: LinearGradient(colors: [
+                Color(0xff70e1f5),
+                Color(0xffffd194),
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            suggestions: _suggestions,
+            searchInputDecoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.auto,
+              labelText: 'SearchField',
+              border: OutlineInputBorder(),
+            ),
+            // hasOverlay: false,
+            hint: 'SearchField Sample 4',
+            maxSuggestionsInViewPort: 4,
+            itemHeight: 45,
+            onTap: (x) {
+              print(x);
+            },
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.3,
+              vertical: 10),
+          child: ElevatedButton(
+              onPressed: () {
+                _formKey.currentState.validate();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Validate Field 2'),
+              )),
+        )
+      ],
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:example/country_model.dart';
 import 'package:flutter/material.dart';
 import 'package:searchfield/searchfield.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter SearchField Demo'),
+      home: MyHomePage(title: 'Countries Demographic'),
     );
   }
 }
@@ -29,210 +30,121 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> _suggestions = [
-    'United States',
-    'Germany',
-    'Washington',
-    'Paris',
-    'Jakarta',
-    'Australia',
-    'India',
-    'Czech Republic',
-    'Lorem Ipsum',
-  ];
-  final List<String> _statesOfIndia = [
-    'Andhra Pradesh',
-    'Assam',
-    'Arunachal Pradesh',
-    'Bihar',
-    'Goa',
-    'Gujarat',
-    'Jammu and Kashmir',
-    'Jharkhand',
-    'West Bengal',
-    'Karnataka',
-    'Kerala',
-    'Madhya Pradesh',
-    'Maharashtra',
-    'Manipur',
-    'Meghalaya',
-    'Mizoram',
-    'Nagaland',
-    'Orissa',
-    'Punjab',
-    'Rajasthan',
-    'Sikkim',
-    'Tamil Nadu',
-    'Tripura',
-    'Uttaranchal',
-    'Uttar Pradesh',
-    'Haryana',
-    'Himachal Pradesh',
-    'Chhattisgarh'
-  ];
-
-  final _searchController = TextEditingController();
-
   @override
   void dispose() {
     _searchController.dispose();
     super.dispose();
   }
 
-  final _formKey = GlobalKey<FormState>();
+  final _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    countries = data.map((e) => Country.fromMap(e)).toList();
+  }
+
+  List<Country> countries = [];
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 20,
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SearchField(
-                suggestions: _suggestions,
-                controller: _searchController,
-                hint: 'SearchField Sample 1',
-                initialValue: _suggestions[2],
-                maxSuggestionsInViewPort: 3,
-                itemHeight: 45,
-                onTap: (x) {
-                  print('selected =$x ${_searchController.text}');
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Form(
-                key: _formKey,
-                child: SearchField(
-                  suggestions: _statesOfIndia,
-                  hint: 'SearchField Sample 2',
-                  searchStyle: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black.withOpacity(0.8),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SearchField(
+                    suggestions: countries.map((e) => e.name).toList(),
+                    controller: _searchController,
+                    hint: 'Search by country name',
+                    maxSuggestionsInViewPort: 4,
+                    itemHeight: 45,
+                    onTap: (x) {
+                      final country = countries.firstWhere((e) => e.name == x);
+                      Navigator.of(context)
+                          .push(MaterialPageRoute<CountryDetail>(
+                              builder: (_) => CountryDetail(
+                                    country: country,
+                                  )));
+                    },
                   ),
-                  validator: (x) {
-                    if (!_statesOfIndia.contains(x) || x.isEmpty) {
-                      return 'Please Enter a valid State';
-                    }
-                    return null;
-                  },
-                  searchInputDecoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.black.withOpacity(0.8),
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: countries.length,
+                    itemBuilder: (_, index) => ListTile(
+                      title: Text(countries[index].name),
+                      onTap: () => Navigator.of(context)
+                          .push(MaterialPageRoute<CountryDetail>(
+                              builder: (_) => CountryDetail(
+                                    country: countries[index],
+                                  ))),
                     ),
                   ),
-                  maxSuggestionsInViewPort: 6,
-                  itemHeight: 50,
-                  onTap: (x) {
-                    print(x);
-                  },
-                ),
-              ),
+                )
+              ],
+            )));
+  }
+}
+
+class CountryDetail extends StatefulWidget {
+  final Country country;
+
+  const CountryDetail({Key key, this.country}) : super(key: key);
+  @override
+  _CountryDetailState createState() => _CountryDetailState();
+}
+
+class _CountryDetailState extends State<CountryDetail> {
+  Widget dataWidget(String key, int value) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.2),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("$key:"),
+          SizedBox(
+            width: 16,
+          ),
+          Text(
+            '$value',
+            style: TextStyle(fontSize: 30),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            alignment: Alignment.center,
+            child: Text(
+              widget.country.name,
+              style: TextStyle(fontSize: 40),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SearchField(
-                maxSuggestionsInViewPort: 5,
-                itemHeight: 40,
-                hint: 'SearchField Sample 3',
-                suggestionsDecoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(8.0),
-                    bottomRight: Radius.circular(8),
-                  ),
-                  border: Border.all(
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                ),
-                suggestionItemDecoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xff9D50BB).withOpacity(0.5),
-                      Color(0xff6E48AA).withOpacity(0.8),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                searchInputDecoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey.withOpacity(0.2),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.white,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  border: OutlineInputBorder(),
-                ),
-                marginColor: Colors.white,
-                suggestions: _suggestions,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SearchField(
-                suggestionStyle: TextStyle(color: Colors.green),
-                suggestionItemDecoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  gradient: LinearGradient(colors: [
-                    Color(0xff70e1f5),
-                    Color(0xffffd194),
-                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-                ),
-                suggestions: _suggestions,
-                searchInputDecoration: InputDecoration(
-                  floatingLabelBehavior: FloatingLabelBehavior.auto,
-                  labelText: 'SearchField',
-                  border: OutlineInputBorder(),
-                ),
-                // hasOverlay: false,
-                hint: 'SearchField Sample 4',
-                maxSuggestionsInViewPort: 4,
-                itemHeight: 45,
-                onTap: (x) {
-                  print(x);
-                },
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.3,
-                  vertical: 10),
-              child: ElevatedButton(
-                  onPressed: () {
-                    _formKey.currentState.validate();
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Validate Field 2'),
-                  )),
-            )
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          dataWidget('Population:', widget.country.population),
+          dataWidget('Density', widget.country.density),
+          dataWidget('Land Area (in Km\'s)', widget.country.landArea)
+        ],
       ),
     );
   }
