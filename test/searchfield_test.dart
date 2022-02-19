7dfad6ba2055a36ba4aa786fce51edbb4d1d315b
mahesh jamdade <maheshmn121@gmail.com>
2022-02-19 13:52:20 +0530

    update tests

--- test/searchfield_test.dart
+++ test/searchfield_test.dart
@@ -38,8 +38,66 @@ void main() {
         suggestions: ['ABC', 'DEF'],
       )));
 
-      final finder = find.byKey(const Key('searchfield'));
+      final finder = find.byType(SearchField);
       expect(finder, findsOneWidget);
+    });
+    testWidgets('Searchfield should set initial Value',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(_boilerplate(
+          child: SearchField(
+        key: const Key('searchfield'),
+        suggestions: ['ABC', 'DEF'],
+        initialValue: 'ABC',
+      )));
+      final finder = find.text('ABC');
+      expect(finder, findsOneWidget);
+      final finder2 = find.text('DEF');
+      expect(finder2, findsNothing);
+    });
+
+    testWidgets(
+        'Searchfield should show searched suggestion and all suggestions on empty searchfield',
+        (WidgetTester tester) async {
+      final TextEditingController controller = TextEditingController();
+      int kOptionsCount = 4;
+      await tester.pumpWidget(_boilerplate(
+          child: SearchField(
+        key: const Key('searchfield'),
+        suggestions: ['ABC', 'DEF', 'GHI', 'JKL'],
+        controller: controller,
+        suggestionState: Suggestion.expand,
+      )));
+      final listFinder = find.byType(ListView);
+      final textField = find.byType(TextFormField);
+      expect(textField, findsOneWidget);
+      expect(listFinder, findsNothing);
+      await tester.tap(textField);
+      await tester.enterText(textField, 'A');
+      await tester.pumpAndSettle();
+      expect(listFinder, findsOneWidget);
+      expect(find.text('ABC'), findsOneWidget);
+      expect(listFinder.evaluate().length, 1);
+      // await tester.enterText(textField, '');
+      // await tester.pumpAndSettle();
+      // expect(listFinder.evaluate().length, kOptionsCount);
+    });
+
+    testWidgets('ListView should be visible when searchfield is tapped',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(_boilerplate(
+          child: SearchField(
+        key: const Key('searchfield'),
+        suggestions: ['ABC', 'DEF', 'GHI'],
+        suggestionState: Suggestion.expand,
+      )));
+      expect(find.byType(TextFormField), findsOneWidget);
+      expect(find.byType(ListView), findsNothing);
+      await tester.tap(find.byType(TextFormField));
+
+      // enter Text isn't required to view the listview, since `suggestionState: Suggestion.expand`
+      await tester.enterText(find.byType(TextFormField), 'a');
+      await tester.pumpAndSettle();
+      expect(find.byType(ListView), findsOneWidget);
     });
   });
 }
