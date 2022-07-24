49dfbbf0e13fba05e1bb289dfcb75aecdbf9eea8
mahesh jamdade <maheshmn121@gmail.com>
2022-07-24 21:42:40 +0530

    add test for dynamic suggestions

--- test/searchfield_test.dart
+++ test/searchfield_test.dart
@@ -260,6 +260,60 @@ void main() {
     expect(focus.hasFocus, false);
   });
 
+  testWidgets('suggestions can be updated in the runtime',
+      (WidgetTester tester) async {
+    var counter = 3;
+    final suggestions = [
+      'suggestion 1',
+      'suggestion 2',
+    ];
+    await tester.pumpWidget(_boilerplate(child: StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SearchField(
+              maxSuggestionsInViewPort: counter,
+              key: const Key('searchfield'),
+              suggestions: suggestions
+                  .map((e) => SearchFieldListItem<String>(e))
+                  .toList(),
+              // hasOverlay: false,
+              suggestionState: Suggestion.expand,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      counter++;
+                      suggestions.add('suggestion $counter');
+                    },
+                  );
+                },
+                child: Text('tap me'))
+          ],
+        );
+      },
+    )));
+    final button = find.byType(ElevatedButton);
+    final listView = find.byType(ListView);
+    final textField = find.byType(TextFormField);
+    expect(textField, findsOneWidget);
+    expect(button, findsOneWidget);
+    expect(listView, findsNothing);
+    await tester.tap(textField);
+    await tester.enterText(textField, '');
+    await tester.pumpAndSettle();
+    expect(listView, findsWidgets);
+    expect(find.text('suggestion $counter'), findsNothing);
+    expect(find.text('suggestion ${counter - 1}'), findsOneWidget);
+    expect(suggestions.length, equals(2));
+    await tester.tap(button);
+    await tester.pumpAndSettle(Duration(seconds: 2));
+    expect(suggestions.length, equals(3));
+    expect(find.text('suggestion $counter'), findsOneWidget);
+  });
+
   group('Searchfield should respect SuggestionState: ', () {
     testWidgets('ListView should be visible when searchfield is tapped',
         (WidgetTester tester) async {
