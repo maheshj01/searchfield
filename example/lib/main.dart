35884bee777c531a3a27cbef44e5fb3add03be80
mahesh jamdade <maheshmn121@gmail.com>
2023-05-02 18:04:33 -0400

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,82 @@
-import 'package:example/dynamic_suggestions.dart.dart';
-import 'package:example/demo.dart';
-import 'package:example/country_search.dart';
 import 'package:flutter/material.dart';
+import 'package:searchfield/searchfield.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: ExampleList());
+      title: 'Flutter App',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: SearchFieldSample(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class ExampleList extends StatelessWidget {
-  const ExampleList({Key? key}) : super(key: key);
+class SearchFieldSample extends StatefulWidget {
+  const SearchFieldSample({Key? key}) : super(key: key);
 
+  @override
+  State<SearchFieldSample> createState() => _SearchFieldSampleState();
+}
+
+class _SearchFieldSampleState extends State<SearchFieldSample> {
+  List<String> suggestions = [
+    'United States',
+    'Germany',
+    'Washington',
+    'Paris',
+    'Jakarta',
+    'Australia',
+    'India',
+    'Czech Republic',
+    'Lorem Ipsum',
+  ];
+  int suggestionsCount = 2;
+  final focus = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('SearchField Demo')),
-        body: Column(
-          children: [
-            ListTile(
-              title: Text('Demo App'),
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => DemoApp()));
-              },
-            ),
-            ListTile(
-              title: Text('Country Search'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => CountrySearch(
-                          title: 'Country List',
-                        )));
-              },
-            ),
-            ListTile(
-              title: Text('Dynamic sample'),
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => DynamicSample()));
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Dynamic sample Demo'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              suggestionsCount++;
+              suggestions.add('suggestion $suggestionsCount');
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        body: Center(
+          child: SearchField(
+            key: const Key('searchfield'),
+            hint: 'Search by country name',
+            suggestions:
+                suggestions.map((e) => SearchFieldListItem<String>(e)).toList(),
+            focusNode: focus,
+            suggestionState: Suggestion.expand,
+            onSuggestionTap: (SearchFieldListItem<String> x) {
+              focus.unfocus();
+            },
+          ),
         ));
   }
 }
